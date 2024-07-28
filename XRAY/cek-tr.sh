780e47a17f627c5b3f94dc8ed38f34af0b4f7a98
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2024-07-29 01:45:14 +0800

    Update cek-tr.sh

--- XRAY/cek-tr.sh
+++ XRAY/cek-tr.sh
@@ -6,11 +6,9 @@ green='\e[0;32m'
 NC='\e[0m'
 echo -n >/tmp/other.txt
 data=($(cat /usr/local/etc/xray/trojanws.json | grep '^###' | cut -d ' ' -f 2 | sort | uniq))
-echo -e "\033[0;34m
-\033[0m"
-echo -e "\\E[0;41;36m     XRAY Trojan WS User Login     \E[0m"
-echo -e "\033[0;34m
-\033[0m"
+echo -e "\e[33m╒════════════════════════════════════════════╕\033[0m"
+echo -e " \E[1;48;39m                XRAY Trojan WS User Login                 \E[0m"
+echo -e "\e[33m╘════════════════════════════════════════════╛\033[0m"
 for akun in "${data[@]}"; do
 	if [[ -z "$akun" ]]; then
 		akun="tidakada"
