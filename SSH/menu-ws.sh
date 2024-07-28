6aa663e37c8f3751df58d6b11b1f72b00496b808
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2024-07-28 20:22:11 +0800

    Update menu-ws.sh

--- SSH/menu-ws.sh
+++ SSH/menu-ws.sh
@@ -5,23 +5,15 @@ P='\e[0;35m'
 B='\033[0;36m'
 N='\e[0m'
 clear
-echo -e "\e[36m
-\033[0m"
+echo -e "\e[33m╒════════════════════════════════════════════╕\033[0m"
 echo -e " \E[0;41;36m             XRAY VMESS WS MENU             \E[0m"
-echo -e "\e[36m
-\033[0m
- [\033[1;36m
-1 \033[0m]  Add XRAY Vmess WS Account
- [\033[1;36m
-2 \033[0m]  Check User Login XRAY Vmess WS
- [\033[1;36m
-3 \033[0m]  Delete XRAY Vmess WS Account
- [\033[1;36m
-4 \033[0m]  Renew XRAY Vmess WS Account
- [\033[1;36m
-5 \033[0m]  Check XRAY Vmess Config
- [\033[1;36m
-6 \033[0m]  Back To Main Menu"
+echo -e "\e[33m╘════════════════════════════════════════════╛\033[0m
+[\033[1;33m•1\033[0m]  Add XRAY Vmess WS Account
+[\033[1;33m•2\033[0m]  Check User Login XRAY Vmess WS
+[\033[1;33m•3\033[0m]  Delete XRAY Vmess WS Account
+[\033[1;33m•4\033[0m]  Renew XRAY Vmess WS Account
+[\033[1;33m•5\033[0m]  Check XRAY Vmess Config
+[\033[1;33m•0\033[0m]  Back To Main Menu"
 echo ""
 echo -e " \033[1;37mPress [ Ctrl+C ]
  To-Exit-Script\033[0m"
@@ -49,7 +41,7 @@ if [[ $(cat /opt/.ver) = $serverV ]] >/dev/null 2>&1; then
 		user-ws
 		read -n1 -r -p "Press any key to continue..."
 		menu
-	elif [[ $x -eq 6 ]]; then
+	elif [[ $x -eq 0 ]]; then
 		clear
 		menu
 	else
