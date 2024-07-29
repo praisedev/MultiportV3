7057226c106544bc32b89bfb6d5f0da47204c2b4
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2024-07-29 12:55:03 +0800

    Update hapus.sh

--- SSH/hapus.sh
+++ SSH/hapus.sh
@@ -4,11 +4,9 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 clear
-echo -e "\033[0;34m
-\033[0m"
-echo -e "\\E[0;41;36m       Delete SSH WS Account       \E[0m"
-echo -e "\033[0;34m
-\033[0m"
+echo -e "\e[33m╒════════════════════════════════════════════╕\033[0m"
+echo -e " \E[1;47;39m       Delete SSH WS Account                \E[0m"
+echo -e "\e[33m╘════════════════════════════════════════════╛\033[0m"
 echo ""
 read -p "Username SSH To Delete : " Pengguna
 if getent passwd $Pengguna >/dev/null 2>&1; then
