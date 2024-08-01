1a484d39553a5b61b9e57e3d9a9cb178471cc40f
KhaiVpn767 <112326124+KhaiVpn767@users.noreply.github.com>
2024-08-02 04:33:21 +0800

    Update ssh-vpn.sh

--- SETUP/ssh-vpn.sh
+++ SETUP/ssh-vpn.sh
@@ -303,7 +303,7 @@ systemctl enable resolvconf.service
 cd /usr/bin
 wget -O limit "https://raw.githubusercontent.com/KhaiVpn767/MultiportV3/main/SSH/limit-speed.sh"
 wget -O bbr "https://raw.githubusercontent.com/KhaiVpn767/KhaiVpn767-TCP-BBR/main/bbr.sh"
-wget -O add-ssh2 "https://raw.githubusercontent.com/KhaiVpn767/multiport/main/add-user/add-ssh.sh”
+wget -O add-ssh "https://raw.githubusercontent.com/KhaiVpn767/multiport/main/add-user/add-ssh.sh”
 wget -O ceklim "https://raw.githubusercontent.com/KhaiVpn767/MultiportV3/main/SSH/ceklim.sh"
 wget -O delete "https://raw.githubusercontent.com/KhaiVpn767/MultiportV3/main/SSH/delete.sh"
 wget -O hapus "https://raw.githubusercontent.com/KhaiVpn767/MultiportV3/main/SSH/hapus.sh"
