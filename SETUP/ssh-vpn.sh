7b34ce07841cc9ce82d3c2f0433020ae4e1fdf60
praisedev <62954475+praisedev@users.noreply.github.com>
2024-12-01 19:07:30 +0700

    Update ssh-vpn.sh

--- SETUP/ssh-vpn.sh
+++ SETUP/ssh-vpn.sh
@@ -247,28 +247,28 @@ fi
 
 clear
 echo
-echo 'Installing DOS-Deflate 0.6'
+#echo 'Installing DOS-Deflate 0.6'
 echo
 echo
-echo -n 'Downloading source files...'
-wget -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
-echo -n '.'
-wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
-echo -n '.'
-wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
-echo -n '.'
-wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
-chmod 0755 /usr/local/ddos/ddos.sh
-cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
-echo '...done'
+#echo -n 'Downloading source files...'
+#wget -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
+#echo -n '.'
+#wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
+#echo -n '.'
+#wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
+#echo -n '.'
+#wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
+#chmod 0755 /usr/local/ddos/ddos.sh
+#cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
+#echo '...done'
 echo
-echo -n 'Creating cron to run script every minute.....(Default setting)'
-/usr/local/ddos/ddos.sh --cron >/dev/null 2>&1
-echo '.....done'
+#echo -n 'Creating cron to run script every minute.....(Default setting)'
+#/usr/local/ddos/ddos.sh --cron >/dev/null 2>&1
+#echo '.....done'
 echo
-echo 'Installation has completed.'
-echo 'Config file is at /usr/local/ddos/ddos.conf'
-echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
+#echo 'Installation has completed.'
+#echo 'Config file is at /usr/local/ddos/ddos.conf'
+#echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
 
 # banner /etc/issue.net
 wget -q -O /etc/issue.net "https://raw.githubusercontent.com/praisedev/MultiportV3/main/OTHERS/issues.net" && chmod +x /etc/issue.net
